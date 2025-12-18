a03c9aa81f8a1857d590f85b2d686a00ff73b02a
chronbook <kingdragon113vn@gmail.com>
2025-12-18 15:20:06 +0700

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,11 +1,11 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("Test");
+var btn = getElementById("Test");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
