69b324f4fab128e38abd38e8acdc7964c0d4661b
chronbook <kingdragon113vn@gmail.com>
2026-01-02 12:11:44 +0700

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,26 +1,27 @@
-function Test()
+function Test() {
   // Get the modal
-  var modal = document.getElementById("myModal");
-  
-  // Get the button that opens the modal
-  var btn = document.getElementById("Test");
-  
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-  
-  // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
+var id_select_apt = document.getElementById("id_select_apt");
+
+// Get the button that opens the modal
+var id_select_apt_button = document.getElementById("id_select_apt_button");
+
+// Get the <span> element that closes the modal
+var select_apt_close = document.getElementsByClassName("select_apt_close")[0];
+
+// When the user clicks the button, open the modal 
+id_select_apt_button.onclick = function() {
+  id_select_apt.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+select_apt_close.onclick = function() {
+  id_select_apt.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    id_select_apt.style.display = "none";
   }
-  
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+}
+}
